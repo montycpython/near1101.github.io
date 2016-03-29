0ea86303db6cd8a9ea13bfea49948b7e4e9bec33
Monty <real.artistry@gmail.com>
2016-03-29 09:06:56 -0400

    Created a base template and three children templates; added a basic sign in function.

--- radapp.py
+++ radapp.py
@@ -1,12 +1,29 @@
 #!/usr/bin/python
 __author__ = 'Montavious'
-from flask import Flask
+from flask import Flask, render_template, request, flash, redirect, url_for
 
 app = Flask(__name__)
+app.secret_key = "4d1ce3e001c8bc4932be3c57d12b8205ad75f38b493a24026d7abf067af3c146"
 
 @app.route("/")
-def hello_world():
-    return "<h1>Hello!!!!! &#x2766;</h1>"
+@app.route("/<name>")
+def hello_world(name=None):
+    "Should serve as the cover of a book or a book store front."
+    return render_template("index.html", name=name)
+    # request, session, g, and get_flashed_messages() are available in every jinja2 template for Flask
+
+@app.route("/signin", methods=["GET","POST"])
+def log_in():
+    error = None
+    if request.method == "POST":
+        if request.form['username'] != 'admin' or \
+            request.form['password'] != 'secret':
+            error = 'Invalid credentials!'
+            flash(error)
+        else:
+            flash('You were successfully logged in!')
+            return redirect(url_for('hello_world', name=request.form["username"].title()))
+    return render_template('login.html', error=error)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
